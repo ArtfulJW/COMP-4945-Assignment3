05f1ee4adefa34dbbd18a81878c3747b9b0c943b
Jay Wang <jwang550@my.bcit.ca>
2023-01-29 18:50:02 -0800

    2nd Push

---
+++ COMP-4549-Assignment3/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using COMP_4549_Assignment3.Models;
+
+namespace COMP_4549_Assignment3
+{
+    public class AppContext : DbContext
+    {
+        public AppContext()
+        : base("Database1")
+        {
+        }
+        public DbSet<Client> Client { get; set; }
+    }
+}
