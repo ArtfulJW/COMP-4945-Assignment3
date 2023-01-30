883370cd146264e622dfc517a18a7ab92bdcba99
Jay Wang <jwang550@my.bcit.ca>
2023-01-29 19:28:47 -0800

    Made changes to DB, Update-Database works now

---
+++ COMP-4549-Assignment3/Migrations/202301300327168_AddJobCollectionClient.cs
@@ -0,0 +1,53 @@
+﻿namespace COMP_4549_Assignment3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddJobCollectionClient : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Jobs",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ClientID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Clients", t => t.ClientID, cascadeDelete: true)
+                .Index(t => t.ClientID);
+            
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        DOE = c.DateTime(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            DropColumn("dbo.Clients", "Address");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Clients", "Address", c => c.String());
+            DropForeignKey("dbo.Jobs", "ClientID", "dbo.Clients");
+            DropIndex("dbo.Jobs", new[] { "ClientID" });
+            DropTable("dbo.Services");
+            DropTable("dbo.Employees");
+            DropTable("dbo.Jobs");
+        }
+    }
+}
